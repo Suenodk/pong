1b3a62737c8a1f3f4e137f57e7ef82dfde3a0742
Suenodk <36229816+Suenodk@users.noreply.github.com>
2022-11-20 08:13:39 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,11 +20,86 @@ const MESSAGE_ENUM = Object.freeze({
 
 const app = uWS
   .App()
-  .get('/*', (res, req) => {
+    .ws("/ws", {
+    // config
+    compression: 0,
+    maxPayloadLength: 16 * 1024 * 1024,
+    idleTimeout: 60,
 
-    /* It does Http as well */
-    res.writeStatus('200 OK').writeHeader('IsExample', 'Yes').end('Hello there!');
-    
+    open: (ws, req) => {
+      ws.username = ws.id = uuidv4();
+
+      // subscribe to topics
+      ws.subscribe(MESSAGE_ENUM.CLIENT_CONNECTED);
+      ws.subscribe(MESSAGE_ENUM.CLIENT_DISCONNECTED);
+      ws.subscribe(MESSAGE_ENUM.CLIENT_MESSAGE);
+
+      // global SOCKETS array created earlier
+      SOCKETS.push(ws);
+
+      // indicate message type so the client can filter with a switch statement later on
+      let selfMsg = {
+        type: MESSAGE_ENUM.SELF_CONNECTED,
+        body: {
+          x,
+          y
+        },
+      };
+
+      let pubMsg = {
+        type: MESSAGE_ENUM.CLIENT_CONNECTED,
+        body: {
+          id: ws.id,
+          name: ws.username,
+        },
+      };
+
+      // send to connecting socket only
+      ws.send(JSON.stringify(selfMsg));
+
+      // send to *all* subscribed sockets
+      app.publish(MESSAGE_ENUM.CLIENT_CONNECTED, JSON.stringify(pubMsg));
+    },
+
+    message: (ws, message, isBinary) => {
+      // decode message from client
+      let clientMsg = JSON.parse(decoder.decode(message));
+      let serverMsg = {};
+
+      switch (clientMsg.type) {
+        case MESSAGE_ENUM.CLIENT_MESSAGE:
+          x = Math.random() * 600;
+          y = Math.random() * 800;
+          serverMsg = {
+            type: MESSAGE_ENUM.CLIENT_MESSAGE,
+            sender: ws.username,
+            body: {x, y},
+          };
+
+          app.publish(MESSAGE_ENUM.CLIENT_MESSAGE, JSON.stringify(serverMsg));
+          break;
+        default:
+          console.log("Unknown message type.");
+      }
+    },
+
+    close: (ws, code, message) => {
+      SOCKETS.find((socket, index) => {
+        if (socket && socket.id === ws.id) {
+          SOCKETS.splice(index, 1);
+        }
+      });
+
+      let pubMsg = {
+        type: MESSAGE_ENUM.CLIENT_DISCONNECTED,
+        body: {
+          id: ws.id,
+          name: ws.name,
+        },
+      };
+
+      app.publish(MESSAGE_ENUM.CLIENT_DISCONNECTED, JSON.stringify(pubMsg));
+    },
   })
   .listen(port, (token) => {
     token
