6d80f6ab7abc1f2fc9c5d007e7ca1df550a3c91f
Suenodk <suenodekort@gmail.com>
2022-11-26 13:44:18 +0100

    service worker support

--- client/js/site.js
+++ client/js/site.js
@@ -1,3 +1,12 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
+
 const screenWidth = 600;
 const screenHeight = 800;
 
