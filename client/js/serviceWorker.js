9a35fa59f892fdc97f61c9c3a07315febcaf56c4
Suenodk <suenodekort@gmail.com>
2023-01-03 19:47:21 +0100

    updated serviceworker

--- client/js/serviceWorker.js
+++ client/js/serviceWorker.js
@@ -1,16 +1,12 @@
-const cacheName = 'Ppong';
+const cacheName = "Ppong";
 
 // Cache all the files to make a PWA
-self.addEventListener('install', e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
+    caches.open(cacheName).then((cache) => {
       // Our application only has two files here index.html and manifest.json
       // but you can add more such as style.css as your app grows
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
     })
   );
 });
@@ -18,12 +14,8 @@ self.addEventListener('install', e => {
 // Our service worker will intercept all fetch requests
 // and check if we have cached the file
 // if so it will serve the cached file
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, { ignoreSearch: true }))
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(async () => {
+    return fetch(event.request);
+  });
 });
